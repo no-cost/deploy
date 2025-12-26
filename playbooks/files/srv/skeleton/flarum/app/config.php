26564de56e3567bb803a67ac6f9e51b28d718590
Kevin Svitač <kevin@svit.ac>
2025-12-26 14:56:29 +0100

    remove mysql host, add to config.php instead for flarum

--- playbooks/files/srv/skeleton/flarum/app/config.php
+++ playbooks/files/srv/skeleton/flarum/app/config.php
@@ -13,11 +13,12 @@ $config = json_decode($jsonContent, true, 512, JSON_THROW_ON_ERROR);
 $defaults = [
     'offline' => false,
     'database' => [
+        'host' => '127.0.0.1',
+        'port' => '3306',
         'driver' => 'mysql',
         'charset' => 'utf8mb4',
         'collation' => 'utf8mb4_unicode_ci',
         'prefix' => '',
-        'port' => '3306',
         'strict' => false,
     ],
     'paths' => [
